cd11876979caa3bcd910b30702f9a6520d5e5d89
cheers8oys <cheers8oys@gmail.com>
2025-11-02 18:21:55 +0900

    fix: 테스트 실패로 인한 로직 변경
    - 불변 리스트에서 sort() 시도 때문에 표출된 UnsupportedOperationException 문제 해결

--- src/main/java/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -1,13 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumberGenerator {
 
     public List<Integer> createLotto() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         lotto.sort(Comparator.naturalOrder());
         return lotto;
     }
